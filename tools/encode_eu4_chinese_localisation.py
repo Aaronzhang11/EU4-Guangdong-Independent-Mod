c2d93f5483ad9bcac45903121b267738d3c3059a
西南亚瓢 <xinanyapiao@xinanyapiaodeMacBook-Air.local>
2026-07-30 23:07:48 +0800

    yangtze bug fixed

--- tools/encode_eu4_chinese_localisation.py
+++ tools/encode_eu4_chinese_localisation.py
@@ -29,6 +29,7 @@ FILES = {
     "gdd_b11_jiangsu_map_readable_utf8.txt": "gdd_b11_jiangsu_map_l_english.yml",
     "gdd_b09_chongqing_map_readable_utf8.txt": "gdd_b09_chongqing_map_l_english.yml",
     "gdd_b03_wangji_map_readable_utf8.txt": "gdd_b03_wangji_map_l_english.yml",
+    "gdd_yangtze_navigation_readable_utf8.txt": "gdd_yangtze_navigation_l_english.yml",
     "gdd_treaty_readable_utf8.txt": "gdd_treaty_l_english.yml",
     "gdd_characters_readable_utf8.txt": "gdd_characters_l_english.yml",
     "gdd_invested_tributary_readable_utf8.txt": "gdd_invested_tributary_l_english.yml",
